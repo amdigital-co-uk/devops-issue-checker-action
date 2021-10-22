cc58eb694726564562f47e59810dd8b7627b5137
Rhodri Hewitson <rhodrih@amdigital.co.uk>
2021-10-22 16:31:35 +0100

    Initial commit of action

---
+++ README.md
@@ -0,0 +1,2 @@
+# code-coverage-action
+Action to validate cobertura branch coverage and fail workflow pipeline if it doesn't meet the specified criteria 
---
+++ index.js
@@ -0,0 +1,6 @@
+const core = require("@actions/core");
+const { action } = require("./src/action");
+
+action().catch((error) => {
+  core.setFailed(error.message);
+});
